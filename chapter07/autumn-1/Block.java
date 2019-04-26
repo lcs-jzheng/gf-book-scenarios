48c231706cb2da91d4504db314cc8dfb4e757273
Jeff Zheng <jzheng@lcs.on.ca>
2019-04-26 14:00:25 -0400

    Changed the game, when the block touches leaf, it will dissapear

--- chapter07/autumn-1/Block.java
+++ chapter07/autumn-1/Block.java
@@ -18,6 +18,26 @@ public class Block extends Actor
         move();
         checkEdge();
         checkMouseClick();
+        checkForLeafCollision();
+    }
+    
+    /**
+     * Remove a Leaf object if we are interacting with it.
+     */
+    private void checkForLeafCollision()
+    {
+        //Get an object reference to a single Leaf from any Leaf objects we are currently interacting
+        //NOTE 1: By placing a type in brackets before the call to getOneIntersectingObject, we **cast** (or force) the type to change from Actor to Leaf
+        //NOTE 2: We can do this because Lead is a subclass of Actor. 
+        Leaf someLeaf = (Leaf)getOneIntersectingObject(Leaf.class);
+        // Check that the someLeaf object actually has a reference to an object
+        //If it is null, that means there was no Leaf intersecting the Blockk this time around
+        if(someLeaf != null)
+        {
+            //someLeaf is NOT null... so now we can remove it
+            World myWorld = getWorld();
+            myWorld.removeObject(someLeaf);
+        }
     }
     
     /**
